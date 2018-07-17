b713e26186f1753bbebf375fab7667cb9c1e0a22
Alex Kim <agkim@lbl.gov>
2018-07-17 16:23:30 -0700

    add animation

--- src/toy.py
+++ src/toy.py
@@ -1,7 +1,10 @@
 #!/usr/bin/env python
 
 import numpy
+import matplotlib
+matplotlib.use("Agg")
 import matplotlib.pyplot as plt
+from matplotlib.animation import FuncAnimation
 
 a = 1.
 k = 1.
@@ -14,19 +17,45 @@ def U(q):
 def qpt(t, q_0, p_0):
     A = q_0
     B = p_0/w
-    return A*numpy.cos(w*t)+B*numpy.sin(w*t),0
+    return A*numpy.cos(w*t)+B*numpy.sin(w*t),-w*A*numpy.sin(w*t)+w*B*numpy.cos(w*t)
 
-n = 10000
+n = 100
 q = [0.5]
 p = [0.1]
 t= 65.24
 
-for i in xrange(n):
+files = []
+
+for i in range(n):
     p_ = numpy.random.normal(0,numpy.sqrt(m))
     qp = qpt(t, q[-1], p_)
     q.append(qp[0])
     p.append(qp[1])
 
+fig, ax = plt.subplots()
+xdata, ydata = [], []
+ln, = plt.plot([], [], 'ro', animated=True)
+
+def init():
+    ax.set_xlim(-5,5)
+    ax.set_ylim(-5,5)
+    return ln,
+
+def update(frame):
+    ln.set_data(q[0:frame], p[0:frame])
+    return ln,
+
+ani = FuncAnimation(fig, update, frames=n,
+                    init_func=init, blit=True)
+
+# Set up formatting for the movie files
+Writer = matplotlib.animation.writers['ffmpeg']
+writer = Writer(fps=15, metadata=dict(artist='Me'), bitrate=1800)
+
+ani.save("temp.mp4",writer=writer)
+qdwqw
+
+plt.clf()
 q=numpy.array(q)
 
 plt.hist(q,normed=True,bins=100)
